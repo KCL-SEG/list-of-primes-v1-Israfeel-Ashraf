577a931591eef408999b8829f681bc55a965afed
Israfeel Ashraf <israfeel.ashraf@kcl.ac.uk>
2022-09-30 13:15:12 +0100

    Finish primes v1

--- primes.py
+++ primes.py
@@ -1,6 +1,37 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+def isPrime(numberToCheck):
+
+    if (numberToCheck == 1): return False
+
+    x = 1
+    while x < numberToCheck:
+        
+        if (numberToCheck % x == 0) and (x != 1 or x == numberToCheck):
+            return False
+
+        elif (x + 1) == numberToCheck:
+            return numberToCheck
+
+        x += 1
+        
+    return None
+
 def primes(number_of_primes):
-    list = []
-    return list
+    
+
+    list1 = []
+    x = 1
+
+    while len(list1) != number_of_primes:
+        if isPrime(x) is not False:
+            list1.append(x)
+
+        x += 1
+            
+    
+    return list1
+
+print(primes(20))
